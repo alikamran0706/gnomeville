180412b6a08c5dfe47215f8533e9cd7dbc80f212
ali.kamran <ali.kamran@clvtechnologies.com>
2024-12-03 19:31:49 +0500

    loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,15 @@
 'use client';
 import './globals.css'
 import { useState } from 'react';
+import { usePathname } from 'next/navigation';
 import Loading from '@/components/Loading';
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [isLoading, setIsLoading] = useState(true);
+  const pathname = usePathname(); 
+  const [isLoading, setIsLoading] = useState(pathname === '/gnome' ? false : true);
   return (
     <html lang="en">
       <body >{
